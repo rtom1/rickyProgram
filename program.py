19be58e22e70c76e877133284ac6c65af6a6ff87
rtom1 <130468237+rtom1@users.noreply.github.com>
2023-11-03 15:16:40 +0000

    first commit

---
+++ program.py
@@ -0,0 +1,9 @@
+def profileData():
+    location = input('where are you from!')
+    name = input('whats your name?')
+    picture = input('upload a picture')
+    Hours = int(input('How many hours would you like to work'))
+    Salary = float(input('How much would you like to make'))
+    print('success! Welcome to indeed.')
+
+profileData()
